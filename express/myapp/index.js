63b9dd6b1566f07e77dc0bdb1caf3ddcac26ec20
DanielRenard <danieljuderenard@gmail.com>
2023-12-02 13:12:03 -0600

    finished lab1

--- express/myapp/index.js
+++ express/myapp/index.js
@@ -1,15 +1,28 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const ports = [3003, 3004, 3005];
+const port2 = 3001;
 
 app.get("/", (req, res) => {
   res.send("Hi! I'm Paul!");
 });
 
 app.get("/test", (req, res) => {
-    res.send("Test!");
-  });
+  res.send("Test!");
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
+});
+
+app.listen(port2, () => {
+  console.log(`Example app listening at http://localhost:${port2}`);
+});
+
+ports.forEach((port) => {
+  console.log(port);
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+  });
 });
